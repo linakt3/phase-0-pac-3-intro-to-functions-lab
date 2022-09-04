845616c6a40a26c2dffdfc44bff775e8204c9c4c
Gloria Paulina Gonzalez Hernandez <ghpaulina3@gmail.com>
2022-09-03 18:29:21 -0600

    commit tree 27bb93341671deede13d15de057292eda35dc292

---
+++ index.js
@@ -0,0 +1,24 @@
+const expect = require("expect/lib");
+const { spyOn } = require("expect/lib");
+
+function shout(string) {
+return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string) {
+console.log(string.toUpperCase());
+}
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+function sayHiToHeadphonedRoommate = string {
+    if (string.toLowerCase()) {return ("I can\'t hear you!")};
+    if ("I can\'t hear you") {return ("YES INDEED!")}
+    if (string.toUpperCase()) {return ("YES INDEED!")};
+    if ("YES INDEED!") {return ("Let\'s have dinner together!")};
+    if ("Let\'s have dinner together!") {return ("I would love to!")};
+};
+console.log(sayHiToHeadphonedRoommate);
+
